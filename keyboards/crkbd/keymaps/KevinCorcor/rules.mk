a272e250656de1fd371ee097726d9f8b07dccf77
unknown <corkeyin@gmail.com>
2024-08-10 11:05:49 +0100

    corne

---
+++ keyboards/crkbd/keymaps/KevinCorcor/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE 	= no     # Mouse keys
+VIA_ENABLE      	= yes    # Enable VIA
+LTO_ENABLE      	= no	 # smaller output
+TAP_DANCE_ENABLE 	= yes
+PERMISSIVE_HOLD 	= yes
+CONVERT_TO			= promicro_rp2040
